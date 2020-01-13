5a483ee3eb1e44a613f202ab2f668929f21c3afc
Rachel Eiting <reiting1@gmail.com>
2020-01-12 19:21:10 -0600

    Update so build will run

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Me from './Me';
 import Projects from './Projects';
 import Work from './Work';
